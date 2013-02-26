b69ee04abcd1dc046258057245c4a84877cafb84
Oscar Ojeda <alfa_omega_96@hotmail.com>
2013-02-25 23:52:40 -0500

    Quest Arreglada: Un Señor de la Guerra atroz
    Fix Quest: Un Señor de la Guerra atroz

---
+++ sql/updates/world/2013_02_25_01_world_event_scripts.sql
@@ -0,0 +1,4 @@
+-- Quest Fixed Un Señor de la Guerra atroz
+DELETE FROM event_scripts WHERE id=17767;
+INSERT INTO event_scripts
+VALUES (17767,0,10,27002,180000,0,2737.611328,427.5159,67.308487,3.162860);
